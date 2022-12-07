b7c22c351adcf8e0c93ae9828a042b42c147e2c5
Artos Web <teodor.at.tod@gmail.com>
2022-12-07 22:57:15 +0200

    add infiite loop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Virtuoso } from 'react-virtuoso';
 import { apiKey } from './apiKey';
 
 function App() {
+  const [photos, setPhotos] = useState([])
+
   const headers = {
     "Content-Type": "application/json",
     "api_key": apiKey
@@ -10,7 +12,6 @@ function App() {
 
   const BASE_URL = `https://petstore.swagger.io/v2/pet/findByStatus?status=available`
 
-  const [photos, setPhotos] = useState([])
 
   const getPhotos = useCallback(async () => {
     const response = await fetch(BASE_URL, { headers })
@@ -26,14 +27,26 @@ function App() {
 
   return (
     <div style={{ width: "100vw", height: "90vh", textAlign: "center" }}>
-      <h1>React Inifinite Scroll Virtuoso</h1>
+      <h1>React Inifinite Scroll</h1>
       <Virtuoso
         data={photos}
         endReached={getPhotos}
         overscan={200}
         itemContent={(index, photo) => {
-          return <h1>{photo.id}</h1>
-          // <img key={index} src={photo.urls.small} alt={`Photo ${index}`} />
+          return (
+            <div className="d-flex justify-content-center">
+              <div className="col-sm-5 my-3 " >
+                <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                <img src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d" alt="" />
+                  <div className="card-body">
+                    <h5 className="card-title text-end h5">Id : {photo.id}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted text-end">Name: {photo.name}</h6>
+                    <p className="card-text text-end">Status : {photo.status}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
         }}
       />
     </div>
