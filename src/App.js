d906032f1e8960592afe725e38b9218d107e07b1
Artos Web <teodor.at.tod@gmail.com>
2022-12-08 09:44:52 +0200

    add querry string limitations

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { apiKey } from './apiKey';
 
 function App() {
   const [photos, setPhotos] = useState([])
+  const [dogs, setDogs] = useState();
 
   const headers = {
     "Content-Type": "application/json",
     "api_key": apiKey
-  }
+  };
 
-  const BASE_URL = `https://petstore.swagger.io/v2/pet/findByStatus?status=available`
+  const BASE_URL = `https://petstore.swagger.io/v2/pet/findByStatus?status=available&count=5`;
 
+  function dogsHandler() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+          setDogs(data);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+}
 
   const getPhotos = useCallback(async () => {
     const response = await fetch(BASE_URL, { headers })
@@ -24,6 +35,11 @@ function App() {
   }, [getPhotos])
 
 
+  useEffect(() => {
+    dogsHandler();
+  }, [])
+
+  console.log(dogs);
 
   return (
     <div style={{ width: "100vw", height: "90vh", textAlign: "center" }}>
@@ -35,9 +51,9 @@ function App() {
         itemContent={(index, photo) => {
           return (
             <div className="d-flex justify-content-center">
-              <div className="col-sm-5 my-3 " >
+              <div className="col-sm-5 my-4 " >
                 <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
-                <img src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d" alt="" />
+                <img src={dogs.message} alt="" />
                   <div className="card-body">
                     <h5 className="card-title text-end h5">Id : {photo.id}</h5>
                     <h6 className="card-subtitle mb-2 text-muted text-end">Name: {photo.name}</h6>
